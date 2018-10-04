3f92a9dfa7828d18f22717094af896f846940cf4
Min Zhang <minzhangcmu@gmail.com>
2018-10-04 10:22:51 -0700

    fix: fix path

--- generate_secrets.sh
+++ generate_secrets.sh
@@ -3,7 +3,7 @@
 for name in '{{cookiepassword}}' '{{encryptionpassword}}' '{{hashingpassword}}'
 do
     value=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 32 | base64`
-    sed -i "" "s/$name/$value/g" templates/screwdriver-api-secrets.yaml
+    sed -i "" "s/$name/$value/g" screwdriver-api-secrets.yaml
 done
 
 openssl genrsa -out jwt.pem 2048
@@ -12,5 +12,5 @@ openssl rsa -in jwt.pem -pubout -out jwt.pub
 privatekey=`cat jwt.pem | base64`
 publickey=`cat jwt.pub | base64`
 
-sed -i "" "s/{{jwtpublickey}}/$publickey/g" templates/screwdriver-api-secrets.yaml
-sed -i "" "s/{{jwtprivatekey}}/$privatekey/g" templates/screwdriver-api-secrets.yaml
+sed -i "" "s/{{jwtpublickey}}/$publickey/g" screwdriver-api-secrets.yaml
+sed -i "" "s/{{jwtprivatekey}}/$privatekey/g" screwdriver-api-secrets.yaml
