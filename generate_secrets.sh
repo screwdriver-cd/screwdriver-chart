f3737dfa29dec12826abf7c97f12139fd398e2d0
ppaul <pritam.paul@verizonmedia.com>
2021-04-30 16:20:57 -0400

    feat: fix delimiter for sed

--- generate_secrets.sh
+++ generate_secrets.sh
@@ -9,7 +9,7 @@ cp screwdriver-api-secrets.tmpl screwdriver-api-secrets.yaml
 
 for name in '{{cookiepassword}}' '{{encryptionpassword}}' '{{hashingpassword}}'; do
   value=$(cat /dev/urandom | env LC_CTYPE=C tr -dc a-zA-Z0-9 | head -c 32 | base64)
-  sed -i.bak "s/$name/$value/g" screwdriver-api-secrets.yaml
+  sed -i.bak "s~$name/$value~g" screwdriver-api-secrets.yaml
 done
 
 for target in 'api' 'queue'; do
@@ -19,11 +19,11 @@ for target in 'api' 'queue'; do
   privatekey=$(cat "${target}-jwt.pem" | base64 | tr -d '\n')
   publickey=$(cat "${target}-jwt.pub" | base64 | tr -d '\n')
 
-  sed -i.bak "s~{{${target}-jwtpublickey}}/$publickey/g" screwdriver-api-secrets.yaml
-  sed -i.bak "s~{{${target}-jwtprivatekey}}/$privatekey/g" screwdriver-api-secrets.yaml
+  sed -i.bak "s~{{${target}-jwtpublickey}}/$publickey~g" screwdriver-api-secrets.yaml
+  sed -i.bak "s~{{${target}-jwtprivatekey}}/$privatekey~g" screwdriver-api-secrets.yaml
 done
 
 scmsettings=$(cat example-scm-settings.json | base64 | tr -d '\n')
-sed -i.bak "s/{{scmsettings}}/$scmsettings/g" screwdriver-api-secrets.yaml
+sed -i.bak "s~{{scmsettings}}/$scmsettings~g" screwdriver-api-secrets.yaml
 
 rm screwdriver-api-secrets.yaml.bak
